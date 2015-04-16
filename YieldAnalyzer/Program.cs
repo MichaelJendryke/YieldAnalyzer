b8b0fab0a85a3086d2d2931d05bc0a8b42c09f14
MichaelJendryke <michael@jendryke.de>
2015-04-16 15:58:34 +0800

    status

--- YieldAnalyzer/Program.cs
+++ YieldAnalyzer/Program.cs
@@ -21,21 +21,37 @@ namespace YieldAnalyzer
            
             DataTable mytable = new DataTable();
             Console.WriteLine(DateTime.Now + " Start to load table");
-            string query = "select top 10 * from weiboDEV.dbo.GEOminimal";
+            string query = "select top 1 * from weiboDEV.dbo.GEOminimal";
             Console.WriteLine(query);
             mytable = SQLServer.readDT(query);
-
-
             disp.dispDT(mytable);
 
-            
             mytable.Columns.Add("geography", typeof(SqlGeography));
             mytable.Columns.Add("geometry", typeof(SqlGeometry));
-
-
+            Console.WriteLine("");
+            Console.WriteLine(mytable.Columns["location"].DataType);
+            Console.WriteLine(mytable.Columns["geography"].DataType);
+            Console.WriteLine(mytable.Columns["geometry"].DataType);
+            
+            DataTable geotable = new DataTable();
+            geotable.Columns.Add("geography", typeof(SqlGeometry));
+            
+            Console.WriteLine("");
             disp.dispDT(mytable);
 
-                        
+
+
+
+
+            DataTable shapesADM2 = new DataTable();
+            Console.WriteLine(DateTime.Now + " Start to load table");
+            string query2 = "select [OBJECTID],[Shape],[NAME_2] from weiboDEV.dbo.GADM_CHN_ADM2";
+            Console.WriteLine(query2);
+            shapesADM2 = SQLServer.readDT(query2);
+            //disp.dispDT(shapesADM2); 
+            Calculations.Intersect(mytable, shapesADM2);
+
+
             Console.ReadLine();
 
         }
@@ -96,6 +112,35 @@ namespace YieldAnalyzer
             myConnection.Close();
 
             return table;
+
+
+        }
+    }
+
+    class Calculations {
+        static public void Intersect(DataTable points, DataTable polygons)
+        {
+            foreach (DataRow row in points.Rows)
+            {
+                Console.WriteLine(row["location"]);
+                //SqlGeography g1 = (SqlGeography)row["location"];
+                SqlGeography g1 = SqlGeography.Parse(row["location"].ToString());
+
+                foreach (DataRow rowS in polygons.Rows)
+                { 
+                //Console.WriteLine("{0,12}", rowS["OBJECTID"].ToString() + " ");
+                //row["location"].STIntersects(rowS["shape"]);
+
+               //SqlGeometry g1 = SqlGeography.Point;
+                    SqlGeography g2 = SqlGeography.Parse(rowS["shape"].ToString());
+                    SqlGeography intersection = g1.STIntersection(g2);
+                    //Console.WriteLine(intersection.ToString());
+
+
+                }
+
+            }
+
 
 
         }
